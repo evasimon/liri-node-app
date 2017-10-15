26d242fb31a184e6110f20f63c2b4c9bf515ddd2
Eva Simon <eva.simon@gmail.com>
2017-10-15 17:16:54 -0500

    adds comments

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-
+// Twitter API keys
 var twitterKeys = {
   consumer_key: 'ucJAP1BJXXpspyqej8wf6QVjN',
   consumer_secret: 'bmvAdy8rMX0m3Evtf0OlypA372Oo3IlIq5fvj6CvBbDC2qyCPc',
@@ -6,11 +6,13 @@ var twitterKeys = {
   access_token_secret: 'jVY2Vj6Tkt71SAL5XIZgXwZUDLayXw95sevaP6TA3qQf4',
 }
 
+// Spotify API keys
 var spotifyKeys = {
 	id: '991c01257f0945009b2a037ed8339d03',
 	secret: '4c08e332a4c147b5b04e1b7bde4709ce'
 }
 
+// exports API keys
 module.exports = {
 	twitterKeys: twitterKeys,
 	spotifyKeys: spotifyKeys
